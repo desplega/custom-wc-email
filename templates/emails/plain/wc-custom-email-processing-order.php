6ad823f38db59a6c9ed098eafe14915f3a54d496
desplega <desplega@desplega.com>
2021-11-09 10:03:36 +0000

    Plain email to the wine cellar

--- templates/emails/plain/wc-custom-email-processing-order.php
+++ templates/emails/plain/wc-custom-email-processing-order.php
@@ -25,19 +25,91 @@ echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 echo sprintf( esc_html__( 'You’ve received the following order from %s:', 'woocommerce' ), esc_html( $order->get_formatted_billing_full_name() ) ) . "\n\n";
 
 /*
- * @hooked WC_Emails::order_details() Shows the order details table.
- * @hooked WC_Structured_Data::generate_order_data() Generates structured data.
- * @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
-* @since 2.5.0
+ *-----------------------------------------------------------------------------------------------------
+ * Following section is a customised version of "email-order-details" and "email-order-items" templates
+ *-----------------------------------------------------------------------------------------------------
  */
-do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
+/* translators: %1$s: Order ID. %2$s: Order date */
+echo wp_kses_post( wc_strtoupper( sprintf( esc_html__( '[Order #%1$s] (%2$s)', 'woocommerce' ), $order->get_order_number(), wc_format_datetime( $order->get_date_created() ) ) ) ) . "\n";
 
-echo "\n----------------------------------------\n\n";
+foreach ( $order->get_items() as $item_id => $item ) :
+	if ( apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
+		$product       = $item->get_product();
+		$sku           = '';
+		$purchase_note = '';
+
+        /**
+         * Skip if category is "Accesorios"
+         */
+        $accessory = false;
+        $terms = get_the_terms($product->id, 'product_cat');
+        if ($terms) {
+            foreach ($terms as $term) {
+                if ($term->slug == 'accesorios') {
+                    $accessory = true;
+                    break;
+                }
+            }
+            if ($accessory)
+                continue;
+        } else {
+            continue; // Unclassified product
+        }
+
+		if ( is_object( $product ) ) {
+			$sku           = $product->get_sku();
+			$purchase_note = $product->get_purchase_note();
+		}
+
+		// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo wp_kses_post( apply_filters( 'woocommerce_order_item_name', $item->get_name(), $item, false ) );
+		if ( $show_sku && $sku ) {
+			echo ' (#' . $sku . ')';
+		}
+		echo ' X ' . apply_filters( 'woocommerce_email_order_item_quantity', $item->get_quantity(), $item );
+		echo "\n";
+		// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
+
+		// allow other plugins to add additional product information here.
+		do_action( 'woocommerce_order_item_meta_start', $item_id, $item, $order, $plain_text );
+		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo strip_tags(
+			wc_display_item_meta(
+				$item,
+				array(
+					'before'    => "\n- ",
+					'separator' => "\n- ",
+					'after'     => '',
+					'echo'      => false,
+					'autop'     => false,
+				)
+			)
+		);
+
+		// allow other plugins to add additional product information here.
+		do_action( 'woocommerce_order_item_meta_end', $item_id, $item, $order, $plain_text );
+	}
+	// Note.
+	if ( $show_purchase_note && $purchase_note ) {
+		echo "\n" . do_shortcode( wp_kses_post( $purchase_note ) );
+	}
+	echo "\n\n";
+endforeach;
+
+echo "==========\n\n";
+
+if ( $order->get_customer_note() ) {
+	echo esc_html__( 'Note:', 'woocommerce' ) . "\t " . wp_kses_post( wptexturize( $order->get_customer_note() ) ) . "\n";
+}
+
+do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text, $email );
+echo "\n";
 
 /*
- * @hooked WC_Emails::order_meta() Shows order meta data.
+ *-----------------------------------------------------------------------------------------------
+ * End of section (customised version of "email-order-details" and "email-order-items" templates)
+ *----------------------------------------------------------------------------------------------
  */
-do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
 
 /*
  * @hooked WC_Emails::customer_details() Shows customer details
